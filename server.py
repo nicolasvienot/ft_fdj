0198ee44930d279be308f4d00ae74916e6dc6fdb
Nicolas Vienot <nvienot@student.42.fr>
2019-11-13 23:49:39 +0100

    [NV] test

--- server.py
+++ server.py
@@ -13,9 +13,7 @@ app.add_api('swagger.yml')
 @app.before_request
 def activate_job():
     def run_job():
-        while True:
-            print("Run recurring task")
-            time.sleep(3)
+		app.add_api('swagger.yml')
 
     thread = threading.Thread(target=run_job)
     thread.start()
